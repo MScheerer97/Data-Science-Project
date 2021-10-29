4a310aa9086f6c82fa0fbedefc8b37cf92ebf1d4
Martin Scheerer <martin-scheerer97@web.de>
2021-10-29 21:52:15 +0200

    First dataset complete. Now: extract nutrients data

--- Data Acquisition.R
+++ Data Acquisition.R
@@ -13,15 +13,17 @@ rm(list = ls())
 
 if (!require("httr")) install.packages("httr")
 if (!require("jsonlite")) install.packages("jsonlite")
+if (!require("dplyr")) install.packages("dplyr")
 
 
 library(httr)
 library(jsonlite)
+library(dplyr)
 
 # Data Acquisition --------------------------------------------------------
 
-## Food Nutrition Data - Using the FoodData Central API Guide by the 
-## U.S. Department of Agriculture
+### Food Nutrition Data - Using the FoodData Central API Guide by the 
+### U.S. Department of Agriculture
 
 source("API Key.R")
 
@@ -29,25 +31,27 @@ source("API Key.R")
 # We start with page one and successively append the following pages
 
 page <- 1
-food_url <- "https://api.nal.usda.gov/fdc/v1/foods/list?"
+food_url <- "https://api.nal.usda.gov/fdc/v1/foods/search?"
 
 food_data <- GET(url = food_url, query = list(api_Key = .key, 
                                               pageNumber = page, 
                                               pageSize = 200, 
                                               dataType = "SR Legacy"
-                                              )) 
+                                              ))
 food_data$url
-
 cont <- content(food_data, as = "text")
-food <- fromJSON(cont)
+food <- fromJSON(cont); length(food$foods)
+foods <- food$foods[, c("description", "lowercaseDescription", "foodCategory", 
+                         "foodNutrients")]
 
 repeat { 
   
   page <- page + 1
   food_data <- GET(url = food_url, query = list(api_Key = .key, 
                                                 pageNumber = page, 
-                                                pageSize = 200, 
-                                                dataType = "SR Legacy"))  
+                                                pageSize = 200,
+                                                dataType = "SR Legacy"
+                                                ))  
   
   if(food_data$status_code != 200) {
     
@@ -57,9 +61,9 @@ repeat {
   }
   
   cont <- content(food_data, as = "text")
-  data <- fromJSON(cont)
+  food <- fromJSON(cont)
   
-  if(length(data) == 0){
+  if(length(food$foods) == 0){
     
     print(paste("Page number", page - 1, "is the last page."))
     break
@@ -68,9 +72,24 @@ repeat {
   
   else {
     
-    food <- rbind(food, data)
+    data <- food$foods[, c("description", "lowercaseDescription", "foodCategory", 
+                           "foodNutrients")]
+    foods <- rbind(foods, data)
     
   }
+  
+  print(page)
+  
   # 3600 requests per hour
   Sys.sleep(1)
 }
+
+# Filtering the data for Vegetables and Fruits
+
+foods <- foods %>%
+  filter(foodCategory %in% c("Vegetables and Vegetable Products", "Fruits and Fruit Juices", 
+                             "Apples", "Other vegetables and combinations", 
+                             "Other fruits and fruit salads", "Dried fruits"))
+
+
+
